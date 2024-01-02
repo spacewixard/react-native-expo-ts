b8aec551ff934172236f4657db0f786a86269e11
Sarthak Verma <skywalker.eth@protonmail.com>
2024-01-02 17:21:21 +0530

    Setting up Jest testing environment

---
+++ components/__tests__/StyledText.test.js
@@ -0,0 +1,10 @@
+import { MonoText } from "../StyledText";
+import { render, screen } from "@testing-library/react-native";
+
+it(`renders correctly`, () => {
+  render(<MonoText>Snapshot test!</MonoText>);
+  expect(screen.getByText("Snapshot test!")).toBeTruthy();
+
+  // const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
+  // expect(tree).toMatchSnapshot();
+});
